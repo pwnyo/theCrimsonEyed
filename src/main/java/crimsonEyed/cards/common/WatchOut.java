8dc5ee045c09b2dfe7928c6ea3a4f63ee4646c30
pwnyo <pwnismaximus@gmail.com>
2022-01-02 00:34:53 -0500

    all cards finished, missing art

---
+++ src/main/java/crimsonEyed/cards/common/WatchOut.java
@@ -0,0 +1,71 @@
+package crimsonEyed.cards.common;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.LockOnPower;
+import crimsonEyed.SasukeMod;
+import crimsonEyed.cards.AbstractDynamicCard;
+import crimsonEyed.characters.TheCrimsonEyed;
+
+import static crimsonEyed.SasukeMod.makeCardPath;
+
+public class WatchOut extends AbstractDynamicCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = SasukeMod.makeID(WatchOut.class.getSimpleName());
+    public static final String IMG = makeCardPath("WatchOut.png");
+    // This does mean that you will need to have an image with the same NAME as the card in your image folder for it to run correctly.
+
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON; //  Up to you, I like auto-complete on these
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;  //   since they don't change much.
+    private static final CardType TYPE = CardType.SKILL;       //
+    public static final CardColor COLOR = TheCrimsonEyed.Enums.SASUKE_BLUE;
+
+    private static final int COST = 1;
+    private static final int BLOCK = 8;
+    private static final int MAGIC = 1;
+    private static final int UPGRADE_PLUS_MAGIC = 1;
+
+    // /STAT DECLARATION/
+
+
+    public WatchOut() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseBlock = block = BLOCK;
+        baseMagicNumber = magicNumber = MAGIC;
+    }
+
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new GainBlockAction(p, block));
+        for (AbstractMonster mo : AbstractDungeon.getCurrRoom().monsters.monsters) {
+            if (!mo.isDeadOrEscaped()) {
+                addToBot(new ApplyPowerAction(mo, p, new LockOnPower(mo, magicNumber)));
+            }
+        }
+    }
+
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBlock(3);
+            upgradeMagicNumber(UPGRADE_PLUS_MAGIC);
+            initializeDescription();
+        }
+    }
+}
